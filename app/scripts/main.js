4a949f221340e154abd73d5c74ecbdc8f81cb948
CEDELLE PHILIPPE <p.cedelle@wcie.fr>
2015-05-22 23:44:03 +0200

    Rebooting towards new design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,28 +3,26 @@
 
 $(function() {
 
-  var $intro = $('.intro'),
-      $title = $('.intro h1'),
-      $content = $('.content'),
-      $name = $('.intro h1 .name');
+  var $content = $('.content');
 
-  $title.typed({
-    strings: ["<span class='text'>Hello,<br> ^800 My name is </span><span class='name'>Philippe Cedelle</span> ^500"],
-    showCursor: false,
-    onStringTyped: typedDone
-  });
 
-  function typedDone() {
-    TweenMax.to($('.intro h1 .text'), 0.5, { opacity: 0,  
-      onComplete: function () {
-        $(this.target[0]).hide();
-        setTimeout(function () {
-          $intro.addClass('done');
-          $title.addClass('done');
-        }, 100);
-      }
-    });
-    TweenMax.to($content, 200, {delay: 0.6, display: 'block'});
-  }
+  var Portfolio = function () {
+    console.log('App instance created');
+  };
+
+  Portfolio.prototype.init = function () {
+    TweenMax.to($content, 1, {delay: 0.6, opacity: '1'});
+  };
+
+  Portfolio.prototype.loop = function(first_argument) {
+    // body...
+  };
+
+  // init
+  //typedDone();
+  var app = new Portfolio();
+  app.init();
+
+
 
 });

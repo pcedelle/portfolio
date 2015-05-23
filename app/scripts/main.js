2d03aecb6d1b2259fd88f670f1a43d0f2cfaa3a7
CEDELLE PHILIPPE <p.cedelle@wcie.fr>
2015-05-23 18:48:08 +0200

    First step done. Need to think about main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,13 @@
 
 $(function() {
 
-  var $content = $('.content');
+  var $window = $(window),
+      $intro = $('.intro'),
+      $logo = $('.logo'),
+      $slash = $('.intro .slash'),
+      $main = $('.main'),
+      $header = $('.main header .container'),
+      $content = $('.main .content');
 
 
   var Portfolio = function () {
@@ -11,7 +17,18 @@ $(function() {
   };
 
   Portfolio.prototype.init = function () {
-    TweenMax.to($content, 1, {delay: 0.6, opacity: '1'});
+    $intro.height($window.height());
+    TweenMax.to($slash, 1.5, {delay: 1, left: '-70%', onComplete: function () {
+      $logo.hide();
+    } }, '<>');
+    TweenMax.to($intro, 0.7, {delay: 2.5, height: '80px', padding: '0', onComplete: this.stageTwo }, '<>');
+  };
+
+  Portfolio.prototype.stageTwo = function () {
+    $intro.hide();
+    $main.show();
+    TweenMax.to($header, 0.3, {delay: 0, opacity: '1'});
+    TweenMax.to($content, 0.3, {delay: 0.5, opacity: '1'});
   };
 
   Portfolio.prototype.loop = function(first_argument) {
@@ -19,10 +36,8 @@ $(function() {
   };
 
   // init
-  //typedDone();
   var app = new Portfolio();
   app.init();
-
 
 
 });
